208555e72a93259b8923bb6c5ba36d46264f0e0d
ArleyRivasG <arrivasg@unipacifico.edu.co>
2022-11-07 12:49:27 -0500

    Ciclos de vida y ngOnChanges a través de @Input()

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
@@ -6,11 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Pagina1Component implements OnInit {
+export class Pagina1Component implements
+             OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+             AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  nombre: string = 'Arley';
 
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor pagina 1');
+   }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
   }
 
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit pagina 1');
+  }
+
+  guardar(){
+
+  }
 }
+
+// Estos se ejecutan cada que se hace necesario verificar el estado de el template 
+//    (cuando algo se dispara, métodos, click en un form enlazado, etc)
+
+// ngDoCheck
+// ngAfterContentChecked
+//  ngAfterViewChecked
